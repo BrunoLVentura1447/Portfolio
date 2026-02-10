14b48f909f941c2f41899cfba0e7552fcf002e1e
BrunoLVentura1447 <brunoluizventura15@gmail.com>
2026-02-09 23:38:51 -0300

    [update] portifolio v1.2 - version final

--- assets/js/acordeon.js
+++ assets/js/acordeon.js
@@ -1,15 +1,15 @@
 
-const acordeonTriggers = document.querySelectorAll('.acordeon .trigger');
+const acordeonTriggers = document.querySelectorAll('.acordeon .trigger')
 
-acordeonTriggers.forEach(trigger => {
-    trigger.addEventListener('click', () => {
-        const acordeonItem = trigger.parentElement;
-        const isOpen = acordeonItem.classList.contains('open');
-        
+acordeonTriggers.forEach((trigger) => {
+    trigger.addEventListener('click', (e) => {
+        const acordeon = trigger.parentElement
+        const isOpen = acordeon.classList.contains('open')
+
         if (isOpen) {
-            acordeonItem.classList.remove('open');
+            acordeon.classList.remove('open')
         } else {
-            acordeonItem.classList.add('open');
+            acordeon.classList.add('open')
         }
-    });
-});
+    })
+})
